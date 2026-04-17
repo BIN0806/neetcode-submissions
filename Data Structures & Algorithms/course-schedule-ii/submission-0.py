aa8c676ce26d9d09015b30cc43a81a720cd85908
Billy Nguyen <bnvinh0808@gmail.com>
2026-04-17 12:47:43 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        from collections import defaultdict
+        pre = defaultdict(list)
+        for c, req in prerequisites: 
+            pre[c].append(req)
+
+        # [class 0] : [class1 ,class2] means u need to take 1 and 2 before 0
+        path = set()
+        result = []
+        def dfs(node):
+            if node in path:
+                return False
+
+            if not pre[node]:
+                result.append(node)
+                return True
+
+            path.add(node)
+            for req in pre[node]:
+                if not dfs(req):
+                    return False
+                    
+            result.append(node)
+            pre[node] = []      
+            path.remove(node)
+
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return result 
