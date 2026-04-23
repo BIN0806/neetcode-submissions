b01d09ced6d6dd74a81b7312a67dafe2cc3f9ab6
Billy Nguyen <bnvinh0808@gmail.com>
2026-04-23 09:19:57 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1) # let dp[i] represent # of ways to get to position i
+        dp[0] = 1
+        res = 0
+        for i in range(len(dp)):
+            if i + 1 < len(dp):
+                dp[i+1] += dp[i]
+            if i + 2 < len(dp):
+                dp[i+2] += dp[i]
+        print(dp)
+        return dp[-1]
