6bbd9ba2162b16fe5fa86e1d7104fe552b5cfa63
Billy Nguyen <bnvinh0808@gmail.com>
2026-04-30 09:58:00 -0700

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1: return nums[0]
+        dp = [0] * n  # most amount of money made by pos i  
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(nums[i] + dp[i-1], dp[i])
+        return max(dp)
