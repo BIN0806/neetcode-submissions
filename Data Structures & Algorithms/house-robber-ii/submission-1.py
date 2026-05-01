1df27cd88647fc993e011511e751d18ac7d7f841
Billy Nguyen <bnvinh0808@gmail.com>
2026-05-01 12:39:14 -0700

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def rob_h(nums):
+            n = len(nums)
+            dp = [0] * (n) # the max amount of money rob by i
+            dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+            for i in range(2, n):
+                dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+            return max(dp)
+            
+        return max(nums[0], rob_h(nums[:-1]), rob_h(nums[1:]))
